b6ec91d0f1e6033e74059773ad5a931c64f6adcb
Roberth-HQ <rocahuqui@gmail.com>
2025-04-18 19:31:49 -0400

    manejo de Local Storage para manejo de arreglos

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,16 +1,28 @@
-import { computed, Injectable, signal } from '@angular/core';
-import { Character } from '../components/interfaces/character.interface';
+import { Character } from './../components/interfaces/character.interface';
+import { computed, effect, Injectable, signal } from '@angular/core';
+
+const loadFromLocalStorage = ():Character[]=> {
+    const characters =localStorage.getItem('characters');
+    return characters ? JSON.parse(characters):[];
+}
 
 @Injectable({providedIn: 'root'})
 
 export class dragonballService {
-    characters=signal(<Character[]>[
-        {id:1, name:'Goku',power:9001},
-        {id:2, name:'Vegeta',power:8001}
-    ]);
+    // characters=signal(<Character[]>[
+    //     {id:1, name:'Goku',power:9001},
+    //     {id:2, name:'Vegeta',power:8001}
+    // ]);
+    characters= signal<Character[]>(loadFromLocalStorage());
+
+    saveToLocalStorage = effect( ()=>{
+        localStorage.setItem('characters',JSON.stringify( this.characters()))
+        //console.log(`characte count ${this.characters().length}`);
+    })
     addCharacter(character:Character){
         this.characters.update((list)=>[...list,character]);
     }
+
     
 }
 //dependice Injection
